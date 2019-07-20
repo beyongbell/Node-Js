c790694c199bf0c6304765fa180b3decfd85bf36
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-07-20 10:17:33 +0700

    Update Code

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
+const path     = require('path')
+const express  = require('express')
+const hbs      = require('hbs')
+const geocode  = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -20,14 +22,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Tinnakorn Choompee'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Tinnakorn Choompee'
+        name: 'Andrew Mead'
     })
 })
 
@@ -35,21 +37,53 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Tinnakorn Choompee'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+        products: []
     })
 })
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Tinnakorn Choompee',
+        name: 'Andrew Mead',
         errorMessage: 'Help article not found.'
     })
 })
@@ -57,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Tinnakorn Choompee',
+        name: 'Andrew Mead',
         errorMessage: 'Page not found.'
     })
 })
